c61edeb2d30680cc5d03f2b4b45ed05fbaec21da
KEITA Modibo <Modykeita2@gmail.com>
2025-11-17 13:18:13 +0100

    mis en place de scrollView

--- H&M/H&M/Controller/HomeViewController.swift
+++ H&M/H&M/Controller/HomeViewController.swift
@@ -11,26 +11,62 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var ContentView: UIView!
     
+    @IBOutlet var scollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view?.backgroundColor = .white
+        view.backgroundColor = .white
         
         guard let content = ContentView else {return}
         
         content.translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([
-            content.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            content.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            content.widthAnchor.constraint(equalToConstant: 200),
-            content.heightAnchor.constraint(equalToConstant: 150)
-        ])
+        NSLayoutConstraint.activate(
+[
+            content.centerYAnchor
+                .constraint(
+                    equalTo: scollView.contentLayoutGuide.centerYAnchor
+                ),
+            content.centerXAnchor.constraint(
+                equalTo: scollView.contentLayoutGuide.centerXAnchor
+            ),
+//            content.widthAnchor
+//                .constraint(
+//                    equalToConstant: scollView.frameLayoutGuide.widthAnchor
+//                ),
+//            content.heightAnchor
+//                .constraint(
+//                    equalToConstant: scollView.frameLayoutGuide.heightAnchor
+//                ),
+            content.leadingAnchor.constraint(
+                equalTo: scollView.contentLayoutGuide.leadingAnchor
+            ),
+            content.trailingAnchor
+                .constraint(
+                    equalTo: scollView.contentLayoutGuide.trailingAnchor
+                ),
+            content.topAnchor
+                .constraint(
+                    equalTo: scollView.contentLayoutGuide.topAnchor
+                ),
+            content.bottomAnchor
+                .constraint(
+                    equalTo: scollView.contentLayoutGuide.bottomAnchor
+                )
+        ]
+)
         
-        var label = UILabel()
+        let label = UILabel()
         label.text = "Salut BG"
         label.translatesAutoresizingMaskIntoConstraints = false
         content.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: content.centerXAnchor),
+            label.centerYAnchor
+                .constraint(equalTo: content.centerYAnchor, constant: 20)
+        ])
         
     }
     
