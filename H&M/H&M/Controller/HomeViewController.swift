630b047760b487c73a6f0d075d3c3c326855b69a
KEITA Modibo <Modykeita2@gmail.com>
2025-11-18 13:06:49 +0100

    nouvelle hstack

--- H&M/H&M/Controller/HomeViewController.swift
+++ H&M/H&M/Controller/HomeViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var ContentView: UIView!
-    
-    @IBOutlet var navigation: UINavigationItem!
     @IBOutlet var scollView: UIScrollView!
+    
+    
+    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,67 +25,9 @@ class HomeViewController: UIViewController {
         
         content.translatesAutoresizingMaskIntoConstraints = false
         
-//        NSLayoutConstraint.activate(
-//[
-//            content.centerYAnchor
-//                .constraint(
-//                    equalTo: scollView.contentLayoutGuide.centerYAnchor
-//                ),
-//            content.centerXAnchor.constraint(
-//                equalTo: scollView.contentLayoutGuide.centerXAnchor
-//            ),
-////            content.widthAnchor
-////                .constraint(
-////                    equalToConstant: scollView.frameLayoutGuide.widthAnchor
-////                ),
-////            content.heightAnchor
-////                .constraint(
-////                    equalToConstant: scollView.frameLayoutGuide.heightAnchor
-////                ),
-//            content.leadingAnchor.constraint(
-//                equalTo: scollView.contentLayoutGuide.leadingAnchor
-//            ),
-//            content.trailingAnchor
-//                .constraint(
-//                    equalTo: scollView.contentLayoutGuide.trailingAnchor
-//                ),
-//            content.topAnchor
-//                .constraint(
-//                    equalTo: scollView.contentLayoutGuide.topAnchor
-//                ),
-//            content.bottomAnchor
-//                .constraint(
-//                    equalTo: scollView.contentLayoutGuide.bottomAnchor
-//                )
-//        ]
-//)
-//        
-//        let label = UILabel()
-//        label.text = "Salut BG"
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        content.addSubview(label)
-//        
-//        NSLayoutConstraint.activate([
-//            label.centerXAnchor.constraint(equalTo: content.centerXAnchor),
-//            label.centerYAnchor
-//                .constraint(equalTo: content.centerYAnchor, constant: 20)
-//        ])
         
-        navigation.rightBarButtonItem = UIBarButtonItem(
-            image: UIImage(systemName: "moon"),
-            style: .plain,
-            target: self,
-            action: #selector(didTapRightBarButton)
-        )
     }
-    
-    @objc func didTapRightBarButton() {
-        // Action for the right bar button
-        let nextVC = HomeViewController()
-        navigationController?.pushViewController(nextVC, animated: true)
-       
-    }
-   
+  
     
 
 
